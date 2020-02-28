b3669f74c7da764a72b2de7597d78bb082d4e67b
XingBiao1992 <823922489@qq.com>
2020-02-28 15:26:27 +0800

    first commit

---
+++ Datastructure/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/13 15:18
+# @Author  : Eric
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ Datastructure/计算1-100.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/13 15:19
+# @Author  : Eric
+# @Site    : 
+# @File    : 计算1-100.py
+# @Software: PyCharm
+
+# 实现方式一
+# sum = 0
+# for  i in range(1,101):
+#     sum += i
+#
+# print(sum)
+
+# 实现方式二
+
+# i = 1
+# n = 100
+# sum = (1 + n) * n / 2
+# print(sum)
+
+
